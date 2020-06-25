0cbf5b077a2c49bf29e91eb69f97708ab8b8375c
fullcycle <dfoderick@gmail.com>
2020-06-25 16:49:21 -0700

    include the payto address separate from the transaction

--- packages/coil-extension/test/jest/wallet.test.ts
+++ packages/coil-extension/test/jest/wallet.test.ts
@@ -6,7 +6,11 @@ describe('wallet functions',() => {
         const w = new Wallet()
         expect(w).toBeInstanceOf(Wallet)
         w.loadWallet('L5o1VbLNhELT6uCu8v7KdZpvVocHWnHBqaHe686ZkMkyszyU6D7n')
-        const nftx = await w.makeAnyoneCanSpendTx(Long.fromNumber(100),'1KUrv2Ns8SwNkLgVKrVbSHJmdXLpsEvaDf')
+        //'1KUrv2Ns8SwNkLgVKrVbSHJmdXLpsEvaDf'
+        const nftx = await w.makeAnyoneCanSpendTx(Long.fromNumber(100))
         expect(w.lastTx.txIns.length).toBe(1)
+        w.logDetailsLastTx()
+        console.log(w.lastTx.toJSON())
+        console.log(nftx)
     })
 })
